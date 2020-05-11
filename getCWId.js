28b43ab825d163748e2100c80bb0f2114c341173
Stosya <48127231+Stosya@users.noreply.github.com>
2020-05-11 12:31:41 +0300

    update getCWId.js

--- getCWId.js
+++ getCWId.js
@@ -1,3 +1,7 @@
-function getCWId() {
-
+function getCWId(courseId) {
+  var courseworks = Classroom.Courses.Courseworks.list(courseId).courseworks;
+  for each (var cw in courseworks) {
+    if(cw.title === "Видео кодеки")
+      return cw.id;
+  }
 }
